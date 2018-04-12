22319c770836fa906b0001563fc907a29bc0a95a
Hridoy Dutta <hridaydutta123@gmail.com>
2018-04-12 20:58:38 +0530

    add working code with apikeys

--- checkBlackListing.py
+++ checkBlackListing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sys
 import requests
 import ConfigParser
+from urlunshort import resolve
 
 if ((len(sys.argv) < 2)):
     print """\
@@ -16,7 +17,8 @@ config.readfp(open('apikeys.txt'))
 urlFile = sys.argv[1]
 
 # Reading csv data using ; as separator
-data = pd.read_csv(urlFile, sep = ';')
+with open(urlFile) as fr:
+	content = fr.readlines()
 
 # GoogleSafeBrowsing Info
 def checkGoogleSafeBrowsing(urlToCheck):
@@ -28,7 +30,7 @@ def checkGoogleSafeBrowsing(urlToCheck):
 	                       'threatEntries': [{'url': urlToCheck}]}}
 	params = {'key': config.get('API Keys', 'googlesafebrowsing_api_key')}
 	response = requests.post(url, params=params, json=payload)
-	print response.json()
+	print "GoogleSafeBrowsing", response.json()
 	return response.json()
 
 # PhishTank Info
@@ -37,36 +39,65 @@ def checkPhishTank(urlToCheck):
         'format':'json',
         'app_key': config.get('API Keys', 'phishtank_api_key')}
 	response = requests.post(url = "http://checkurl.phishtank.com/checkurl/", data = data)
-	print response.json()
-	return response.json()
-
+	# print "PhishTank", response.json()
+	isSpam = False
+	if 'results' in response.json():
+		return response.json()['results']['in_database']
+	else:
+		return isSpam
 # Virus Total Info
 def checkVirusTotal(urlToCheck):
 	try:
 		params = {'apikey': config.get('API Keys', 'virustotal_api_key'), 'url': urlToCheck}
 		response = requests.post('https://www.virustotal.com/vtapi/v2/url/scan', data=params)
-		print response.json()
+		# print "VirusTotal", response.json()
 		return response.content
 	except:
 		pass
 
 # Web-of-Trust Info
 def checkWOT(urlToCheck):
-	print urlToCheck
 	params = {'hosts': urlToCheck, 'callback':	'process', 'key': config.get('API Keys', 'wot_api_key')}
 	response = requests.get(url='http://api.mywot.com/0.4/public_link_json2', params=params)
-	print response.content
-	return response.content
+	# print "WOT", response.content
+	isSpam = False
+	try:
+		content = eval(response.content[8:-1])
+		print content
 
+		repu = []
+		conf = []
+		if repu:
+			if (sum(repu)/len(repu)) > 60 and (sum(conf)/len(conf) < 10):
+				isSpam = True
+		for keys in content:
+			repu.append(content[keys]['0'][0])
+			repu.append(content[keys]['1'][0])
+			repu.append(content[keys]['2'][0])
+			repu.append(content[keys]['4'][0])
+			conf.append(content[keys]['0'][1])
+			conf.append(content[keys]['1'][1])
+			conf.append(content[keys]['2'][1])
+			conf.append(content[keys]['4'][1])
+				# repu.append(vals['1'])
+	except:
+		pass
+	return isSpam
 
 result = {}	
 
 # Iterate each row of data
-for index, row in data.iterrows():
-	result[row['url']] = {}
-	result[row['url']]['googleSafeBrowsing'] = checkGoogleSafeBrowsing(row['url'])
-	result[row['url']]['PhishTank'] = checkPhishTank(row['url'])
-	result[row['url']]['VirusTotal'] = checkVirusTotal(row['url'])
-	result[row['url']]['wot'] = checkWOT(row['url'])
+for lines in content:
+	url1 = lines.split("\t")[1].replace("\n","")
+	urlLink = resolve(url1)
+	print urlLink
+	result[urlLink] = {}
+	# result[urlLink]['googleSafeBrowsing'] = checkGoogleSafeBrowsing(urlLink)
+	phishtank_res = checkPhishTank(urlLink)
+	virusTotal_res = checkVirusTotal(urlLink)
+	wot_res = checkWOT(urlLink)
+	
+	with open('finalUrlSpam.csv','a+') as fw:
+		fw.write(lines.split("\t")[0] + "," + lines.split("\t")[1].replace("\n","") + "," + str(phishtank_res) + "," + str(wot_res) + "\n")
 
 
